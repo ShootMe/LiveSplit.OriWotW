e5f73caf34df127eb4171c09978e20a5025a1f17
tekoppar <57131048+tekoppar@users.noreply.github.com>
2020-10-18 01:30:39 +0200

    Prologue and control scheme switching fixes
    Fixes the timer stopping in prologue and the timer micro stopping when you perform the janky slowdown, done by forcing the game to control switch every frame which causes the FPS to drop too low which pauses the timer

---
+++ Memory/GameSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace LiveSplit.OriWotW {
+    public enum ControlScheme: byte {
+        Controller = 0,
+        KeyboardAndMouse = 1,
+        Keyboard = 2,
+        Switch = 3
+}
+
+    [StructLayout(LayoutKind.Explicit, Size = 16, Pack = 1)]
+    public struct GameSettings {
+        [FieldOffset(0x0)]
+        public IntPtr Instance;
+        [FieldOffset(0x94)]
+        public ControlScheme m_currentControlSchemes;
+        [FieldOffset(0x88)]
+        public IntPtr m_unlockedCutscenes;
+        [FieldOffset(0xA8)]
+        public IntPtr m_hudEnabled;
+        [FieldOffset(0x58)]
+        public IntPtr m_language;
+    }
+}
