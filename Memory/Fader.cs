9b22aefec5edbb3fe05d06daff4d786bafc59f67
ShootMe <birreller@gmail.com>
2022-01-18 08:15:27 -0600

    qtm fix

--- Memory/Fader.cs
+++ Memory/Fader.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LiveSplit.OriWotW {
+﻿namespace LiveSplit.OriWotW {
     public enum FadeState {
         Null = -1,
         FadeToBlack = 0,
